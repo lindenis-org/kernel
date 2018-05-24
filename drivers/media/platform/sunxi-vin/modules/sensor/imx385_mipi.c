b75abf9bae9574fa63a0d857bd382ca87693073f
Sugar <shuge@allwinnertech.com>
2018-05-24 17:28:01 +0800

    clk: csi support get the 37.152MHz

--- drivers/media/platform/sunxi-vin/modules/sensor/imx385_mipi.c
+++ drivers/media/platform/sunxi-vin/modules/sensor/imx385_mipi.c
@@ -569,6 +569,7 @@ static struct sensor_format_struct sensor_formats[] = {
 
 static struct sensor_win_size sensor_win_sizes[] = {
 
+	/*
 	{
 	 .width = 1936,
 	 .height = 1096,
@@ -590,6 +591,7 @@ static struct sensor_win_size sensor_win_sizes[] = {
 	 .regs_size = ARRAY_SIZE(sensor_4lane_1080P30_regs),
 	 .set_size = NULL,
 	 },
+	 */
  
 	 {
 	  .width = 1920,
